b2f0ebfb5fafa0ecd881fcb8286f2b8d17665e8e
Josue <josue.n.rivera@outlook.com>
2024-02-20 21:23:02 -0500

    Attempt 2 to building quad + code to return quad plot

--- larp/quad.py
+++ larp/quad.py
@@ -22,59 +22,70 @@ class QuadTree():
     def __init__(self, field: PotentialField,
                  minimum_sector_length:float = 5.0,
                  boundaries:Union[np.ndarray, List[float]] = np.arange(0.2, 0.8, 0.2),
-                 size:Optional[float] = None) -> None:
+                 size:Optional[float] = None,
+                 build_tree:bool = False) -> None:
         
         self.field = field
         self.min_sector_size = minimum_sector_length
-        self.size = size if size is not None else np.min(self.field.size)
+        self.size = size if size is not None else np.max(self.field.size)
 
         self.boundaries = np.sort(np.array(boundaries))[::-1]
+        self.n_zones = len(self.boundaries) + 1
+        self.__zones_rad_ln = -np.log(self.boundaries)
         self.ZONEToMaxRANGE = np.append([1.0, 1.0], self.boundaries)
         self.ZONEToMinRANGE = np.append(self.boundaries[0:1], self.boundaries, [0.0])
-        self.__zones_rad_ln = -np.log(np.append([1.0], self.boundaries))
-        self.n_zones = len(self.boundaries) + 1
 
         self.root = None
-        self.leaves = []
+        self.leaves:List[QuadNode] = []
+
+        if build_tree:
+            self.build()
+
+    def mark_leaf(self, quad:QuadNode) -> None:
+
+        self.leaves.append(quad)
+        quad.leaf = True
 
     def __approximated_PF_zones__(self, center_point:Point, size:float, filter_idx:Optional[List[int]] = None) -> Tuple[List[int], np.ndarray]: 
+        zones = np.ones(len(filter_idx), dtype=int) * self.n_zones
 
-        dist_sqr = self.field.squared_dist_list([center_point], filted_idx=filter_idx).ravel()
+        dist_sqr = self.field.squared_dist_list([center_point], filted_idx=filter_idx, scaled=False).ravel()
+        zone0_select = dist_sqr <= (size*size)/2.0
+        zones[zone0_select] = 0
+
+        dist_sqr = self.field.squared_dist_list([center_point], filted_idx=filter_idx[~zone0_select]).ravel()
         dist_sqr_bins = (size*size)/2.0 + np.sqrt(2)*size*np.sqrt(self.__zones_rad_ln) + self.__zones_rad_ln
 
-        zones = np.digitize(dist_sqr, dist_sqr_bins, right=True)
+        zones[~zone0_select] = np.digitize(dist_sqr, dist_sqr_bins, right=True) + 1
 
-        return zones, dist_sqr
+        return zones
 
     def build(self) -> Optional[QuadNode]:
 
         def dfs(center_point:Point, size:float, filter_idx:Optional[List[int]]) -> QuadNode:
             quad = QuadNode(center_point=center_point, size=size)
 
-            zones, dist_sqr = self.__approximated_PF_zones__(center_point=center_point, size=size, filter_idx=filter_idx)
+            zones = self.__approximated_PF_zones__(center_point=center_point, size=size, filter_idx=filter_idx)
             quad.boundary_zone = zone = min(zones)
             quad.boundary_max_range = self.ZONEToMaxRANGE[zone]
             
             size2 = size/2.0
             if size2 <= self.min_sector_size or zone == self.n_zones:
-                # stop subdividing if next size is too small or the active zones are too far away or range cannot be passed
-                self.leaves.append(quad)
+                # stop subdividing if next size is too small or the active zones are too far away
+                self.mark_leaf(quad)
                 return quad
             
-            upper_range, lower_range = self.ZONEToMaxRANGE[zone], self.ZONEToMinRANGE[zone]
-            
-            # TODO: 
-            # 1) Check if other range can exist within size boundary. If not, stop subdividing
+            if zone > 0:
+                # stop subdiving if sphere does not leave zone 0
+                lower_range = self.ZONEToMinRANGE[zone]
+                rgjs_idx = filter_idx[zones == zone]
 
-            ### Attempt 1: Will likely not work
-            if 2*size**2 < -np.log(upper_range) - np.log(lower_range) - np.sqrt(np.log(upper_range)*np.log(lower_range)):
-                # stop subdividing if size is small enough 
+                vectors = self.field.repulsion_vectors([center_point], filted_idx=rgjs_idx).reshape(-1, 2)
+                uni_vectors = vectors/np.linalg.norm(vectors, axis=1)
 
-                self.leaves.append(quad)
-                return quad
-            
-            ### Atempt 2: check for unscaled distance inside circle (zone 0), and scaled outside (zone 1+)
-            # -----
+                if (self.field.eval(center_point + uni_vectors*size2) <= lower_range).any():
+                    self.mark_leaf(quad)
+                    return quad
 
             size4 = size2/2.0
             new_filter_idx = filter_idx[zones < self.n_zones]
@@ -88,10 +99,10 @@ class QuadTree():
         self.root = dfs(self.field.center_point, self.size, np.arange(len(self.field)))
         return self.root
     
-    def to_line_collection(self):
-        # TODO: return segmented lines for matplotlib's LineCollection
-        pass 
-
+    def to_boundary_lines_collection(self):
+        lines = [quad.to_boundary_lines() for quad in self.leaves]
+        
+        return [path for line in lines for path in line]
 
 class QuadNode():
 
@@ -101,6 +112,7 @@ class QuadNode():
     def __init__(self, center_point:Point, size:float) -> None:
         self.center_point = center_point
         self.size = size
+        self.leaf = False
         self.boundary_zone:int = 0
         self.boundary_max_range:float = 1.0
 
@@ -131,5 +143,18 @@ class QuadNode():
         else:
             idx = self.chdToIdx[idx] if not isinstance(idx, int) else idx
             self.children[idx] = value
+    
+    def to_boundary_lines(self) -> Tuple[np.ndarray, np.ndarray]:
+        size2 = self.size/2.0
+        offset = np.array([
+            [-1.0, 1.0],
+            [ 1.0, 1.0],
+            [ 1.0,-1.0],
+            [-1.0,-1.0],
+            [-1.0, 1.0],
+        ]) * size2
+        path = self.center_point + offset
+
+        return path[:, 0], path[:, 1]
 
 

db869c06abca3f7d2832cbd01a1c51fedbd2ba3e
Josue <josue.n.rivera@outlook.com>
2024-05-04 12:23:17 -0400

    init io file

---
+++ larp/io.py
@@ -0,0 +1,15 @@
+
+from larp.field import PotentialField
+
+"""
+Author: Josue N Rivera
+"""
+
+def fromFeatureCollectionGeoJSON(geojson: dict):
+
+    features = geojson["features"]
+
+    return PotentialField()
+
+def loadFile(file: str):
+    pass

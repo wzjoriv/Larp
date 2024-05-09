Patch gradient bug

larp/field.py
@@ -192,7 +192,7 @@ class MultiLineStringRGJ(LineStringRGJ):
         self.grad_matrix = self.inv_repulsion + self.inv_repulsion.T
         
         self.lines_n = sum([len(coords)-1 for coords in self.coordinates])
-        self.points_in_line_pair = np.concatenate([[coords[:-1], coords[1:]] for coords in self.coordinates], axis=1).swapaxes(0, 1) #TODO: Check correctness
+        self.points_in_line_pair = np.concatenate([[coords[:-1], coords[1:]] for coords in self.coordinates], axis=1).swapaxes(0, 1)
 
     def get_center_point(self) -> np.ndarray:
 
@@ -324,7 +324,7 @@ class GeometryCollectionRGJ(RGJGeometry):
         _, dist_idxs = self.squared_dist(x, reference_idx=True, **kwargs)
 
         repulsion_vector = self.repulsion_vector(x, min_dist_select = True, **kwargs)
-        return - self.eval(x=x).reshape(-1, 1) * (np.einsum('ijk,ik->ij', self.grad_matrix[dist_idxs], repulsion_vector)) # TODO: check correctness
+        return - self.eval(x=x).reshape(-1, 1) * (np.einsum('ijk,ik->ij', self.grad_matrixes[dist_idxs], repulsion_vector)) # TODO: check correctness
     
     def toRGeoJSON(self) -> RGeoJSONObject:
         if type(self.RGJType) == type(None): 

test/test_rgj.py
@@ -181,9 +181,17 @@ def test_geometry_collection_rgj():
 
     values = rgj.eval([(80.0, 10.0), (11, 81), (70.0, 80.0), (40, 8)])
 
-    assert ((values[0] - 1)**2).sum() < 1e-5, "Unexpected repulsion vector"
-    assert ((values[1] - 1)**2).sum() < 1e-5, "Unexpected repulsion vector"
-    assert ((values[2] - 1)**2).sum() < 1e-5, "Unexpected repulsion vector"
-    assert ((values[3] - np.exp(-4/9))**2).sum() < 1e-5, "Unexpected repulsion vector"
+    assert ((values[0] - 1)**2).sum() < 1e-5, "Unexpected potential field evaluation"
+    assert ((values[1] - 1)**2).sum() < 1e-5, "Unexpected potential field evaluation"
+    assert ((values[2] - 1)**2).sum() < 1e-5, "Unexpected potential field evaluation"
+    assert ((values[3] - np.exp(-4/9))**2).sum() < 1e-5, "Unexpected potential field evaluation"
+
+    grads = rgj.gradient([(80.0, 10.0), (11, 81), (70.0, 80.0), (40, 8)])
+    grad_line = rgj.rgjs[3].gradient(np.array([(40, 8)]))[0]
+
+    assert ((grads[0] - np.array([0.0]*2))**2).sum() < 1e-5, "Unexpected gradient vector"
+    assert ((grads[1] - np.array([0.0]*2))**2).sum() < 1e-5, "Unexpected gradient vector"
+    assert ((grads[2] - np.array([0.0]*2))**2).sum() < 1e-5, "Unexpected gradient vector"
+    assert ((grads[3] - grad_line)**2).sum() < 1e-5, "Unexpected gradient vector"
 
 test_geometry_collection_rgj()
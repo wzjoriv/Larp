407f4d2b3cdb007f421c7736e5a423d07cef6c8a
Josue <josue.n.rivera@outlook.com>
2024-05-09 20:39:46 -0400

    Add fromGeoJSON

--- larp/io.py
+++ larp/io.py
@@ -29,16 +29,13 @@ def saveRGeoJSON(field:PotentialField, file:str, return_bbox=False):
     with open("sample.json", "w") as outfile:
         json.dump(field.toRGeoJSON(return_bbox=return_bbox), outfile)
 
-def loadGeoJSONFile(file: str, size_offset = 0.0):
+def fromGeoJSON(geojson: dict, size_offset = 0.0):
 
     """
     Converts a GeoJSON into an RGeoJSON
 
     _Note_: All polygons will be converted to line strings.
     """
-
-    with open(file=file) as f:
-        geojson = json.load(f)
 
     def __prune_polygons__(geojson:dict):
         if geojson["type"].lower() == "polygon":
@@ -65,6 +62,13 @@ def loadGeoJSONFile(file: str, size_offset = 0.0):
         __prune_polygons__(geojson["features"][idx]["geometry"])
 
     return fromRGeoJSON(geojson, size_offset=size_offset)
+
+def loadGeoJSONFile(file: str, size_offset = 0.0):
+
+    with open(file=file) as f:
+        geojson = json.load(f)
+
+    return fromGeoJSON(geojson, size_offset=size_offset)
 
 def projectCoordinates(field: PotentialField, from_crs="EPSG:4326", to_crs="EPSG:26917"):
     raise NotImplementedError

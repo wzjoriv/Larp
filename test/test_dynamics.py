6544ff78deaaf38c0095357ebf40c65bfbda4e6b
Josue <josue.n.rivera@outlook.com>
2025-05-08 00:01:22 +0800

    Add unit test for whl dynamics

---
+++ test/test_dynamics.py
@@ -0,0 +1,32 @@
+
+import numpy as np
+import sys
+
+sys.path.append("../larp")
+import larp.dynamics
+import numpy as np
+import scipy.integrate as integrate
+
+"""
+Author: Josue N Rivera
+
+"""
+
+def test_whl_dynamics():
+
+    dynamics = larp.dynamics.WMRDynamics(1.0)
+
+    def f(t:np.ndarray, x:np.ndarray):
+        t = np.reshape(t, (-1, 1))
+        x = x.reshape(-1, dynamics.first_order_state_n)
+        u = np.sin(t / 2).reshape(-1, 1).repeat(2, axis=1)
+
+        return dynamics.f(t, x, u)[0]
+
+    t_span = (0, 10)
+    t = np.linspace(*t_span, 10)
+    x0 = np.array([1, 2, 1])
+    
+    integrate.solve_ivp(f, t_span, x0, t_eval=t)
+
+test_whl_dynamics()

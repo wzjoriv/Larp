1c3b88477ef48c2f769556b23ebcf7057cb95e1d
Josue <josue.n.rivera@outlook.com>
2025-05-27 13:43:51 +0800

    Expand network unit test

--- test/pp/test_network.py
+++ test/pp/test_network.py
@@ -8,7 +8,7 @@ Author: Josue N Rivera
 
 """
 
-def test_quad_on_simple_pf():
+def test_network_on_simple_pf():
     point_rgjs = [{
         'type': "Point",
         'coordinates': [50, 50], 
@@ -26,7 +26,32 @@ def test_quad_on_simple_pf():
                                   edge_bounds=np.arange(0.2, 0.8, 0.2))
     
     network = larp.pp.QuadNetwork(quadtree=quadtree, build_network=True)
+
+test_network_on_simple_pf()
+
+def test_refresh_of_network():
+
+    point_rgjs = [{
+        'type': "Point",
+        'coordinates': [50, 50], 
+        'repulsion': [[5, 0], [0, 5]]
+    },{
+        'type': "Point",
+        'coordinates': [60, 60], 
+        'repulsion': [[5, 0], [0, 5]]
+    }]
+
+    field = larp.PotentialField(size=50, center_point=[55, 55], rgjs=point_rgjs)
+    quadtree = larp.quad.QuadTree(field=field,
+                                  build_tree=True,
+                                  minimum_length_limit=5,
+                                  edge_bounds=np.arange(0.2, 0.8, 0.2))
     
+    network = larp.pp.QuadNetwork(quadtree=quadtree, build_network=True)
+
+    qfield = larp.QPotentailField(quadtree)
+    qfield.delRGJ([1])
     
-if __name__ == "__main__":
-    test_quad_on_simple_pf()
+    network.refresh()
+
+test_refresh_of_network()
